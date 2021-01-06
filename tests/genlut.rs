162d27a9df4d25fe107ae23852c91420981b51ae
yvt <i@yvt.jp>
2021-01-07 01:34:14 +0900

    test(genlut): make the table row of `genlut` dynamic

--- tests/genlut.rs
+++ tests/genlut.rs
@@ -6,31 +6,39 @@ fn init() {
 }
 
 #[quickcheck_macros::quickcheck]
-fn qc_genlut_lut8x16(out_row: usize, mut indices: Vec<u8>, mut values: Vec<u8>) -> TestResult {
+fn qc_genlut_lut8x16(
+    table_row: usize,
+    out_row: usize,
+    mut indices: Vec<u8>,
+    mut values: Vec<u8>,
+) -> TestResult {
     values.resize_with(64, u8::default);
     indices.resize_with(32, u8::default);
     let out_row = out_row % 8;
+    let table_row = table_row % 8;
+    if table_row == 0 {
+        return TestResult::discard();
+    }
 
     indices.resize_with(64, u8::default); // extra for loading
 
     log::debug!("values = {:x?}", values);
     log::debug!("indices = {:x?}", indices);
+    log::debug!("table_row = {:x?}", table_row);
     log::debug!("out_row = {:x?}", out_row);
 
     let mut got = [0u8; 64];
     let all_x;
     unsafe {
         amx::enable();
-        amx::load512_x(values.as_ptr(), 7); // TODO: make this dynamic
+        amx::load512_x(values.as_ptr(), table_row);
         amx::load512_x(indices.as_ptr(), 0); // TODO: make this dynamic
         amx::ops::op_in::<22>(
             ((out_row as u64) << 20)
                 | (1 << 53)
                 | (1 << 55)
                 | (1 << 56)
-                | (1 << 60)
-                | (1 << 61)
-                | (1 << 62),
+                | ((table_row as u64) << 60),
         );
         amx::store512_x(got.as_mut_ptr(), out_row);
         all_x = std::mem::transmute::<_, [[u64; 8]; 8]>(amx::read_x());
